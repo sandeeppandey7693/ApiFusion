5a3dd39d9adf343fa75a8fe00ea303f758e1e1ce
Sav-hub <onlytest901@gmail.com>
2024-10-06 11:40:54 +0530

    removed some cofig

--- src/main/java/org/QA/reports/ExtentReportUtils.java
+++ src/main/java/org/QA/reports/ExtentReportUtils.java
@@ -4,11 +4,11 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.QA.generic.FrameworkGlobalVar;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import java.lang.reflect.Method;
-import org.QA.
 
 public class ExtentReportUtils {
     static ExtentTest testCase;
---
+++ src/test/java/TestClassAPI.java
@@ -0,0 +1,12 @@
+import org.QA.APIConnection.RequestConnect;
+import org.QA.generic.FrameworkGlobalVar;
+import org.testng.annotations.Test;
+
+public class TestClassAPI {
+    @Test
+    public void sampleTestApi(){
+
+        System.out.println("" + FrameworkGlobalVar.projectData);
+//        RequestConnect.makeRequest("")
+    }
+}
